b0b40c8f726317a6cd1e4ab356c5bba6aabec6cc
MikeM0s010v <152646704+MikeM0s010v@users.noreply.github.com>
2023-12-02 21:24:47 +0300

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-# Do not forget to make this script executable with chmod 755 cd.sh
-
-# Change directory to YOUR venv
 echo "Executing a bash command"
-cd /path/to/venv
-
-# Check
-pwd
-echo $$
-
-# Running py-file
+cd /home/mike/venv
+source bin/activate
 python alert.py
+
+
